4ad7a4cf08128773b24ee943ee836259fa3bcf46
Uday Tiwari <udaytiwari360@outlook.com>
2025-07-05 18:39:48 +0530

    Added some effects in /signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,15 @@
+import { WavyBackground } from "@/components/ui/wavy-background";
+
 function Signin() {
+  return (
+    <>
+      <WavyBackground
+        waveOpacity={0.2}
+        className="max-w-4xl mx-auto h-full"
+        colors={["#bffb24", "#f59e42", "#f472b6", "#a78bfa", "#38bdf8"]}
+      ></WavyBackground>
+    </>
+  );
 }
 
 export default Signin;
